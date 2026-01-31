84e42fd8ddbf47ef307921f9097521174131f7eb
Giorgi Kankia <gkankia@gmail.com>
2026-01-31 19:26:51 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -428,8 +428,7 @@ async function initDashboard() {
             <div class="footer-top">
                 <div class="last-update">
                     <span class="last-update-label">ბოლო განახლება:</span>
-                    <span class="last-update-value">${formatDate(data[data.length - 1].date)}</span><br>
-                    <span class="last-update-label">მონაცემების ციტირება:</span>
+                    <span class="last-update-value">${formatDate(data[data.length - 1].date)}</span>
                 </div>
                 <div class="footer-actions">
                     <button class="footer-button" onclick="openMethodModal()">
